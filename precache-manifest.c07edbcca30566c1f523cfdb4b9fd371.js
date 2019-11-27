1578bd9ee5737261c3c2d6c0ee520bbd9e2368bc
Paulo <pauloeduardo2709@gmail.com>
2019-11-27 01:40:03 -0300

    Updates

---
+++ precache-manifest.c07edbcca30566c1f523cfdb4b9fd371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc79cc80aa18f14b5c6890116198101e",
+    "url": "/ReactGradeAvg/index.html"
+  },
+  {
+    "revision": "f8c2478d7e81e32ece8e",
+    "url": "/ReactGradeAvg/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "495782cdc79be6fe2335",
+    "url": "/ReactGradeAvg/static/css/main.8c247060.chunk.css"
+  },
+  {
+    "revision": "f8c2478d7e81e32ece8e",
+    "url": "/ReactGradeAvg/static/js/2.65f95a9b.chunk.js"
+  },
+  {
+    "revision": "495782cdc79be6fe2335",
+    "url": "/ReactGradeAvg/static/js/main.8b7e1606.chunk.js"
+  },
+  {
+    "revision": "43bee173564c99152c27",
+    "url": "/ReactGradeAvg/static/js/runtime-main.58d60930.js"
+  }
+]);
